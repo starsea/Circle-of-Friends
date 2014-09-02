e4445e0538ed3a536e7def3bce8f8ae8ca5cd4eb
starsea <starsea820@gmail.com>
2014-09-03 00:24:11 +0800

    redis 集成

---
+++ application/library/Local/Cache/RedisManager.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * @file
+ *
+ * Redis Connection
+ */
+
+namespace Local\Cache;
+
+use Exception;
+
+class RedisManager
+{
+
+    /**
+     * 链接
+     *
+     * @var array
+     */
+    protected static $connections = array();
+
+    /**
+     * 配置
+     *
+     * @var array
+     */
+    protected static $redisInfo = array();
+
+    /**
+     * 是否已经手工设置过配置
+     *
+     * @var bool
+     */
+    protected static $isConfiguration = false;
+
+    //设置连接信息
+    final public static function setConfig($target, array $config = array())
+    {
+        if (is_string($target)) {
+            self::$redisInfo[$target] = $config;
+        } elseif (is_array($target)) {
+            self::$redisInfo = $target + self::$redisInfo; //?如何定位
+        }
+        self::$isConfiguration = true;
+    }
+
+
+    /**
+     * @desc 获取链接
+     * @param string $target
+     * @return Redis resource
+     */
+    final public static function getConnection($target = 'default')
+    {
+        if (!isset(self::$connections[$target])) {
+            self::$connections[$target] = self::openConnection($target);
+        }
+
+        return self::$connections[$target];
+    }
+
+    //打开链接
+    final public static function openConnection($target)
+    {
+        if (empty(self::$redisInfo)) {
+            self::parseConnectionInfo();
+        }
+
+        $connection = new Redis();
+        if (isset(self::$redisInfo[$target])) {
+            $info = self::$redisInfo[$target];
+
+        } else {
+            $info = array(
+                'host'     => '127.0.0.1',
+                'port'     => 6379,
+                'timeout'  => 0,
+                'database' => 0,
+                'password' => '',
+                'options'  => array(),
+            );
+        }
+        try {
+            $info['timeout'] = isset($info['timeout']) ? $info['timeout'] : 0;
+
+            $connection->connect($info['host'], $info['port'], $info['timeout']);
+
+            if (isset($info['password'])) {
+                $connection->auth($info['password']);
+            }
+
+            if (isset($info['database'])) {
+                $connection->select($info['database']);
+            }
+
+            if (!empty($info['options'])) {
+                foreach ($info['options'] as $k => $v) {
+                    $connection->setOption($k, $v);
+                }
+            }
+
+
+        } catch (Exception $e) {
+            $connection = null;
+        }
+
+        return $connection;
+    }
+
+    //关闭链接
+    public static function closeConnection($target = null)
+    {
+        if (isset($target)) {
+            if (isset(self::$connections[$target])) {
+                self::$connections[$target]->close();
+                self::$connections[$target] = null;
+                unset(self::$connections[$target]);
+            }
+        } else {
+            foreach (self::$connections as $target => $connection) {
+                self::$connections[$target]->close();
+                self::$connections[$target] = null;
+                unset(self::$connections[$target]);
+            }
+        }
+    }
+
+    //解析配置信息 注意不同的框架解析不同的东西
+    final public static function parseConnectionInfo()
+    {
+//        global $redises;
+//        if (!self::$isConfiguration) {
+//            $redisInfo = is_array($redises) ? $redises : array();
+//            self::$redisInfo = $redisInfo;
+//        }
+    }
+
+
+}
